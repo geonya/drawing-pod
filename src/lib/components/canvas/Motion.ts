792ef06cfe77ec29d4d57d2031415f6caab0f70d
geonya <geony8410@gmail.com>
2023-03-10 13:49:03 +0900

    motion refactorinig // preventcanvasexit

--- src/lib/components/canvas/Motion.ts
+++ src/lib/components/canvas/Motion.ts
@@ -79,12 +79,50 @@ export class Motion {
   onDrawingEnd() {
     this.canvas.isDrawingMode = false
   }
+  onPreventCanvasExit(e: fabric.IEvent<MouseEvent>) {
+    if (!e.target) return
+    if (!this.canvas) return
+
+    const objWidth = e.target.width! * e.target.scaleX!
+    const objHeight = e.target.height! * e.target.scaleY!
+    if (e.target.top && e.target.top < 0) {
+      e.target.top = 0
+    }
+    if (e.target.left && e.target.left < 0) {
+      e.target.left = 0
+    }
+    if (
+      e.target.left &&
+      objWidth &&
+      this.canvas.width &&
+      e.target.left + objWidth > this.canvas.width
+    ) {
+      e.target.left = this.canvas.width - objWidth
+    }
+    if (
+      e.target.top &&
+      objHeight &&
+      this.canvas.height &&
+      e.target.top + objHeight > this.canvas.height
+    ) {
+      e.target.top = this.canvas.height - objHeight
+    }
+  }
+  onDelete() {
+    const activeObject = this.canvas.getActiveObject()
+    if (activeObject) {
+      this.canvas.remove(activeObject)
+    }
+  }
   onKeyDown(e: KeyboardEvent) {
     if (e.key === ' ') {
-      this.onDraggingStart()
+      this.onChangeMotionState(MotionState.DRAGGING)
     }
     if (e.key === 'Escape') {
-      motionState.set(MotionState.DEFAULT)
+      this.onChangeMotionState(MotionState.DEFAULT)
+    }
+    if (e.key === 'Delete' || e.key === 'Backspace') {
+      this.onDelete()
     }
   }
   onKeyUp(e: KeyboardEvent) {
